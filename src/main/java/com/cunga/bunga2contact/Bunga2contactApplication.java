9356963fc274b5ddad0eab161b138a868c77fd9b
Roberto Delrosso <robertodelrosso@Robertos-iMac.local>
2020-02-21 16:09:58 +0100

    fist project commit

---
+++ src/main/java/com/cunga/bunga2contact/Bunga2contactApplication.java
@@ -0,0 +1,13 @@
+package com.cunga.bunga2contact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bunga2contactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bunga2contactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cunga/bunga2contact/Bunga2contactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cunga.bunga2contact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bunga2contactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
